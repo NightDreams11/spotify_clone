e2ac76b49b72167ee8ccca69a0ce4d43e083becb
Ross <rost090909@gmail.com>
2024-12-07 17:56:24 -0600

    set up Auth and Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react"
 import { Button } from "./components/ui/button"
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500 text-5xl">hi</h1>
-      <Button>Click me</Button>
-    </>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   )
 }
 
