d671e9ff960d739632fe4c39268ae3c217b8979d
Ross <rost090909@gmail.com>
2024-12-21 21:07:11 -0600

    set up song controller

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 import { clerkClient } from "@clerk/express"
 
-export const portectRoute = async (req, res, next) => {
+export const protectRoute = async (req, res, next) => {
   if (!req.auth.userId) {
     return res
       .status(401)
