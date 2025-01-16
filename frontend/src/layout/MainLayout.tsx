b84561fa9212833fb72983fbf95cb6aceeb83ab9
Ross <rost090909@gmail.com>
2025-01-16 17:43:52 -0600

    added friends activity layout, chat store

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/resizable"
 import { Outlet } from "react-router-dom"
 import { LeftSidebar } from "./components/LeftSidebar"
+import { FriendsActivity } from "./components/FriendsActivity"
 
 export const MainLayout = () => {
   const isMobile = false
@@ -40,7 +41,7 @@ export const MainLayout = () => {
           maxSize={25}
           collapsedSize={0}
         >
-          Right sidebar
+          <FriendsActivity />
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -30,7 +30,7 @@ export const AlbumPage = () => {
 
   return (
     <div className="h-full ">
-      <ScrollArea className="h-full">
+      <ScrollArea className="h-full rounded-md">
         {/* Main Content */}
         <div className="relative min-h-full">
           {/* background gradient */}
